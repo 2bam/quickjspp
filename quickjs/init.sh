26ae8beaa3f3b468574915feab505c10b733e5c1
nitram <8461554+2bam@users.noreply.github.com>
2025-07-04 23:11:50 +0200

    Rollback patches and use --directory for git apply

--- quickjs/init.sh
+++ quickjs/init.sh
@@ -5,6 +5,6 @@ cd $(dirname $0)/..
 
 git subtree add --squash --prefix=quickjs/quickjs https://github.com/bellard/quickjs.git 458c34d29d0d262f824ea1c0e01aa0e3790669da
 
-git apply --verbose -- quickjs/patches/*.patch
+git apply --verbose --directory=quickjs/quickjs-- quickjs/patches/*.patch
 git add .
 git commit -m "Apply patches to initialized quickjs impl"
